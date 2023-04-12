e72e98342e35766af975d3417acbfca59bc21af4
Elyse Montano <elysemontano.dev.com>
2023-04-12 10:02:55 -0700

    adds index fetch and displays apartments

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-
 import ApartmentEdit from "./pages/ApartmentEdit"
 import ApartmentIndex from "./pages/ApartmentIndex"
 import ApartmentNew from "./pages/ApartmentNew"
@@ -12,13 +11,25 @@ import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 
 const App = (props) => {
+  const [apartments, setApartments] = useState([])
+
+  useEffect(() => {
+    readApartments()
+  }, [])
+
+  const readApartments = () => {
+    fetch("/apartments")
+    .then((response) => response.json())
+    .then((payload) => setApartments(payload))
+    .catch((error) => console.log(error))
+  }
   return (
     <>
       <BrowserRouter>
         <Header {...props}/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/apartmentindex" element={<ApartmentIndex />} />
+          <Route path="/apartmentindex" element={<ApartmentIndex apartments={apartments} />} />
           <Route path="/apartmentshow" element={<ApartmentShow />} />
           <Route path="/apartmentnew" element={<ApartmentNew />} />
           <Route path="/apartmentedit" element={<ApartmentEdit />} />
--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import {Card, CardBody, Button, CardTitle} from "reactstrap"
 
-const ApartmentIndex = () => {
+const ApartmentIndex = ({apartments}) => {
   return (
-    <div>ApartmentIndex</div>
+    <>
+      <h1>Apartment Listings</h1>
+      <div className='apartments'>
+        {apartments.map((apartment, index) => {
+          return (
+            <Card
+              key={index}
+              style={{
+                width: '18rem'
+              }}
+            >
+              <img
+                alt={apartment.address}
+                src={apartment.image}
+              />
+              <CardBody>
+                <CardTitle tag="h5">
+                  {apartment.address},{apartment.city}, {apartment.state}
+                </CardTitle>
+                <Button>
+                  Button
+                </Button>
+              </CardBody>
+            </Card>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
