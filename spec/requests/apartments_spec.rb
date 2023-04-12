0a67990804c5ea59e02581037a397e75471d7c46
Elyse Montano <elysemontano.dev.com>
2023-04-12 14:14:30 -0700

    adds new form and controller for create

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -12,4 +12,32 @@ RSpec.describe "Apartments", type: :request do
       expect(apartment.length).to eq 1
     end
   end
+
+  describe "POST/create" do 
+    it "creates an apartment" do 
+      apartment_params = {
+        apartment: {
+          bedrooms: 5, bathrooms: 1.5, address: '123 fourth street', city: 'Miami', state: 'FL', square_footage: 600, price: 300000, utilities: 'washer, dryer, gas, and electric', pets: true, image: 'https://images.unsplash.com/photo-1638945657499-a7e1de359849?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', user_id: user.id
+        }
+      }
+
+      post '/apartments', params: apartment_params
+
+      expect(response).to have_http_status(200)
+      apartment = Apartment.first
+      expect(apartment.address).to eq '123 fourth street'
+    end
+  end
+
+  it "does not create an apartment without an address" do 
+    apartment_params = {
+        apartment: {
+          bedrooms: 5, bathrooms: 1.5, city: 'Miami', state: 'FL', square_footage: 600, price: 300000, utilities: 'washer, dryer, gas, and electric', pets: true, image: 'https://images.unsplash.com/photo-1638945657499-a7e1de359849?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', user_id: user.id
+        }
+      }
+      post "/apartments", params: apartment_params 
+      expect(response).to have_http_status(422)
+      json = JSON.parse(response.body)
+      expect(json['address']).to include "can't be blank"
+  end
 end
